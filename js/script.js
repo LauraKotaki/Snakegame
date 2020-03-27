3004ff661f167c8506ba6e1ee0f3cd95dc6d5d2b
Laura Kotaki <62550500+LauraKotaki@users.noreply.github.com>
2020-03-27 18:25:38 -0300

    Criado movimento da Cobrinha

--- js/script.js
+++ js/script.js
@@ -6,17 +6,42 @@ Snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
+
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box); //vai desenhar onde vai ocorrer o jogo
 }
+
+
 function criarCobrinha(){
-    for (i-0; i < Snake.length; i++){
+    for (i=0; i < Snake.length; i++){
         context.fillStyle = "green";
-        context.fillRect(Snake[i].x, Snake[i].y, box, box]);
+        context.fillRect(Snake[i].x, Snake[i].y, box, box);
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarjogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = Snake[0].x;
+    let snakeY = Snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    Snake.pop();
+
+    let newhead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    Snake.unshift(newhead);
+
+  }
+let jogo = setInterval(iniciarjogo, 100); // intervalo de 100 ms para iniciar o jogo. 
 
